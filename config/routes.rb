89f862340fae03e30a6badc33122f92f1562747d
Nahid1911 <nahid.raihan@gmail.com>
2023-09-12 01:31:07 +0600

    Add template for recipes and recipe

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  resources :recipe_foods
-  resources :recipes
-  resources :foods
-  resources :users
+  resources :users, only: [:index, :show, :new, :create, :destroy] do
+    resources :recipes, only: [:index, :show, :new, :create, :destroy]
+    resources :recipe_foods, only: [:index, :show, :new, :create, :destroy] do 
+      resources :foods
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root 'foods#index'
+  root 'users#index'
 end
