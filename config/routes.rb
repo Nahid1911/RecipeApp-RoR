582a21251849a9c9be2abcf5f8188d60bebf2092
Bisaso Ronald <bronnie313@gmail.com>
2023-09-12 11:10:35 +0300

    add route for sign in and login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  # devise_scope :user do
+  #   root to: 'users#index'
+  # end
   resources :foods
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
