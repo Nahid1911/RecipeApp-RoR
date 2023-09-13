7166fd3b656d27232a1a0e0df14ed60bf06f5f38
Bisaso Ronald <bronnie313@gmail.com>
2023-09-13 12:20:51 +0300

    add modification to foods controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
 
   has_many :recipes, dependent: :destroy
+  has_many :foods, dependent: :destroy
 end
