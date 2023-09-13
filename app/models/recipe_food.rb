2b178e310c958a53b1c3aeddc1ec599f5ea7cead
Nahid1911 <nahid.raihan@gmail.com>
2023-09-14 04:52:34 +0600

    Add functin to add item in the recipe

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -1,4 +1,4 @@
 class RecipeFood < ApplicationRecord
-  belongs_to :user
+  belongs_to :food
   belongs_to :recipe
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_13_174657) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_13_202259) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,12 +27,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_13_174657) do
 
   create_table "recipe_foods", force: :cascade do |t|
     t.integer "quantity"
-    t.bigint "user_id", null: false
+    t.bigint "food_id", null: false
     t.bigint "recipe_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["food_id"], name: "index_recipe_foods_on_food_id"
     t.index ["recipe_id"], name: "index_recipe_foods_on_recipe_id"
-    t.index ["user_id"], name: "index_recipe_foods_on_user_id"
   end
 
   create_table "recipes", force: :cascade do |t|
@@ -45,6 +45,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_13_174657) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_recipes_on_user_id"
+  end
+
+  create_table "tables", force: :cascade do |t|
+    t.string "recipe_food"
+    t.integer "quantity"
+    t.bigint "food_id", null: false
+    t.bigint "recipe_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["food_id"], name: "index_tables_on_food_id"
+    t.index ["recipe_id"], name: "index_tables_on_recipe_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -66,7 +77,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_13_174657) do
   end
 
   add_foreign_key "foods", "users"
+  add_foreign_key "recipe_foods", "foods"
   add_foreign_key "recipe_foods", "recipes"
-  add_foreign_key "recipe_foods", "users"
   add_foreign_key "recipes", "users"
+  add_foreign_key "tables", "foods"
+  add_foreign_key "tables", "recipes"
 end
