e8599173714b8c3b0e4682d72de9ff3778cecfc2
Bisaso Ronald <bronnie313@gmail.com>
2023-09-13 12:42:24 +0300

    add modification to the foods and user controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -8,7 +8,7 @@ class FoodsController < ApplicationController
 
   def show
     @food = Food.find(params[:id])
- end
+  end
 
   def new
     @food = Food.new
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,22 @@
+# Rails.application.routes.draw do
+#   devise_for :users
+#   resources :foods
+#   resources :users
+#   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+#   # Defines the root path route ("/")
+#   # root "articles#index"
+#   root 'foods#index'
+# end
+
 Rails.application.routes.draw do
   devise_for :users
   resources :foods
-  resources :users
+  resources :users, only: [:index, :show, :new, :create, :destroy] do
+    resources :recipes, only: [:index, :show, :new, :create, :destroy]
+    resources :recipe_foods, only: [:index, :show, :new, :create, :destroy] 
+    resources :foods, only: [:index, :show, :new, :create, :destroy] 
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
