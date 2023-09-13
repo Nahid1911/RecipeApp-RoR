e23dda85a3006623a734fe84750997ab1e178e8b
Nahid1911 <nahid.raihan@gmail.com>
2023-09-13 23:45:01 +0600

    Add functionality in toggle button

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -58,6 +58,15 @@ class RecipesController < ApplicationController
     end
   end
 
+  def toggle_public
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(public: !@recipe.public)
+  
+    # Redirect to a relevant page, e.g., the recipe's show page
+    redirect_to recipe_path(@recipe.id)
+  end
+  
+
   # private
   #   # Use callbacks to share common setup or constraints between actions.
   #   def set_recipe
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,11 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :foods
-  resources :recipes, only: [:index, :show, :new, :create, :destroy]
+  resources :recipes, only: [:index, :show, :new, :create, :destroy] do
+    member do
+      post 'toggle_public'
+    end
+  end
   resources :users, only: [:index, :show, :new, :create, :destroy] do
     resources :recipe_foods, only: [:index, :show, :new, :create, :destroy] 
     resources :foods, only: [:index, :show, :new, :create, :destroy] 
