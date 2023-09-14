ecd0365b8e8935333b7c1704bb0f095d10c19630
Nahid1911 <nahid.raihan@gmail.com>
2023-09-14 16:02:18 +0600

    Link recipe_food to individual recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,6 +9,7 @@ class RecipesController < ApplicationController
   # # GET /recipes/1 or /recipes/1.json
   def show
     @recipe = Recipe.find(params[:id])
+    @recipe_food = @recipe.recipe_food
   end
 
   # # GET /recipes/new
