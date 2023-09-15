24b52bf66327de69f98ce7516a381f001c9de787
Nahid1911 <nahid.raihan@gmail.com>
2023-09-15 10:48:53 +0600

    Add ingredients_count and Ingerdients_value column in Recipes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_13_202259) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_15_042838) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,6 +44,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_13_202259) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "ingredient_counter"
+    t.decimal "ingredient_value"
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
