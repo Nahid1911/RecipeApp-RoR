8becd73f8423efc6bb934f024fd98b995aee0907
Nahid1911 <nahid.raihan@gmail.com>
2023-09-15 12:30:23 +0600

    Did some  adjustment in the links

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -6,26 +6,26 @@ class RecipeFood < ApplicationRecord
   after_save :update_recipe_ingredient_value
   after_destroy :update_recipe_ingredient_value_on_destroy
   after_destroy :update_recipe_ingredient_counter
-  
+
   def update_recipe_ingredient_counter
     recipe.update(ingredient_counter: recipe.recipe_foods.count)
   end
 
   def update_recipe_ingredient_value
     self.quantity ||= 0
-    self.food.price ||= 0
+    food.price ||= 0
 
-    ingredient_value = self.quantity * self.food.price
-    total_ingredient_value = self.recipe.ingredient_value.to_f + ingredient_value
-    self.recipe.update(ingredient_value: total_ingredient_value)
+    ingredient_value = self.quantity * food.price
+    total_ingredient_value = recipe.ingredient_value.to_f + ingredient_value
+    recipe.update(ingredient_value: total_ingredient_value)
   end
 
   def update_recipe_ingredient_value_on_destroy
     self.quantity ||= 0
-    self.food.price ||= 0
+    food.price ||= 0
 
-    ingredient_value = self.quantity * self.food.price
-    total_ingredient_value = self.recipe.ingredient_value.to_f - ingredient_value
-    self.recipe.update(ingredient_value: total_ingredient_value)
+    ingredient_value = self.quantity * food.price
+    total_ingredient_value = recipe.ingredient_value.to_f - ingredient_value
+    recipe.update(ingredient_value: total_ingredient_value)
   end
 end
